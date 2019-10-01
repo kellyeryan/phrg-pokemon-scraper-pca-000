97c16bcfa971ebde259a7a15195a5b21fc0a3e3a
kellyeryan <kellyeryan@gmail.com>
2019-10-01 13:45:26 -0400

    Finish pokemon lab

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+require 'pry'
+
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @db = db
+    @name = name
+    @type = type
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type)
+      VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    all.find do |pokemon_id|
+      pokemon_id.id == id
+    end
+  end
+
 end
+
